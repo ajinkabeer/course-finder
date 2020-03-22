0564f2077a981d4832f38f98bd1126430c3d761b
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-22 10:08:33 +0530

    Add basic components

---
+++ src/components/About/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>
+      This app uses React, Redux, React Router for ultra-responsive web apps
+    </p>
+  </div>
+);
+
+export default AboutPage;
