50142a98bf69bc66bbfe8afd06f18329d7a2ee93
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-23 23:19:04 +0530

    Add load author redux functions

--- src/components/Courses/CoursesPage.js
+++ src/components/Courses/CoursesPage.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
+    const { courses, authors } = this.props;
+    if (courses.length === 0) {
+      this.loadCourses();
+    }
+    if (authors.length === 0) {
+      this.loadAuthors();
+    }
+  }
+
+  loadCourses = () => {
     this.props.actions.loadCourses().catch(error => {
       alert("Loading courses failed" + error);
     });
-  }
+  };
+
+  loadAuthors = () => {
+    this.props.actions.loadAuthors().catch(error => {
+      alert("Loading authors failed" + error);
+    });
+  };
 
   render() {
     return (
@@ -24,18 +41,31 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   actions: PropTypes.object.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
