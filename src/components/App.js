adc66464ffa7689021c651e6aabdd5bec6df5fff
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-26 12:47:55 +0530

    Render toast after form submission

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Header from "./Common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./Courses/CoursesPage";
 import ManageCoursePage from "./Courses/ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
         <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
 }
--- src/components/Courses/ManageCoursePage.js
+++ src/components/Courses/ManageCoursePage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
 import Spinner from "../Common/Spinner";
+import { toast } from "react-toastify";
 
 function ManageCoursePage({
   courses,
@@ -47,6 +48,7 @@ function ManageCoursePage({
     event.preventDefault();
     setSaving(true);
     saveCourse(course).then(() => {
+      toast.success("Course saved.");
       history.push("/courses");
     });
   }
