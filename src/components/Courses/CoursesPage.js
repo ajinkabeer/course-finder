94900ce67f6666d413301122b4cb881816c6d723
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-27 12:11:51 +0530

    Switch fetch to async await function

--- src/components/Courses/CoursesPage.js
+++ src/components/Courses/CoursesPage.js
@@ -36,11 +36,13 @@ class CoursesPage extends React.Component {
     });
   };
 
-  handleDeleteCourse = course => {
+  handleDeleteCourse = async course => {
     toast.success("Course deleted");
-    this.props.actions.deleteCourse(course).catch(error => {
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
       toast.error("Delete Failed" + error.message, { autoClose: false });
-    });
+    }
   };
 
   render() {
