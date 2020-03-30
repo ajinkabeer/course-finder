f8b99a214a274008f6bc848532f9e74f705ede9e
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-30 11:02:55 +0530

    Setup webpack for prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,74 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
+
+process.env.NODE_ENV = "production";
+
+module.exports = {
+  mode: "production", //webpack knows to run in dev mode. Sets node env in dev and disables production only features
+  target: "web", // For browser, can also be set to node for apps running in node
+  devtool: "source-map", //Source map let us see original code in the browser
+  entry: "./src/index", //entry point of the app
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/", //public url of the output directory when its referenced in the browser
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.png",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    })
+  ],
+  //tell webpack what files to handle
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("cssnano")],
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
