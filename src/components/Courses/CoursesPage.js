df1d5c74269ba934b6259e8c65b1ea10c29fec52
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-14 12:07:00 +0530

    Refactor class-based course page with hooks

--- src/components/Courses/CoursesPage.js
+++ src/components/Courses/CoursesPage.js
@@ -1,106 +1,79 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
-import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
-class CoursesPage extends React.Component {
-  state = {
-    redirectToAddCoursePage: false
-  };
+function CoursesPage() {
+  const [redirectToAddCoursePage, setRedirect] = useState(false);
 
-  componentDidMount() {
-    const { courses, authors, actions } = this.props;
+  const authors = useSelector((state) => state.authors);
+  const loading = useSelector((state) => state.apiCallsInProgress > 0);
+  const courses = useSelector((state) =>
+    state.authors.length === 0
+      ? []
+      : state.courses.map((course) => {
+          return {
+            ...course,
+            authorName: state.authors.find((a) => a.id === course.authorId)
+              .name,
+          };
+        })
+  );
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    handleLoadingCourses();
+  }, []);
+
+  const handleLoadingCourses = () => {
     if (courses.length === 0) {
-      actions.loadCourses().catch(error => {
+      dispatch(courseActions.loadCourses()).catch((error) => {
         alert("Loading courses failed" + error);
       });
     }
 
     if (authors.length === 0) {
-      actions.loadAuthors().catch(error => {
+      dispatch(authorActions.loadAuthors()).catch((error) => {
         alert("Loading authors failed" + error);
       });
     }
-  }
+  };
 
-  handleDeleteCourse = async course => {
+  const handleDeleteCourse = async (course) => {
     toast.success("Course deleted");
     try {
-      await this.props.actions.deleteCourse(course);
+      await dispatch(courseActions.deleteCourse(course));
     } catch (error) {
       toast.error("Delete failed. " + error.message, { autoClose: false });
     }
   };
 
-  render() {
-    return (
-      <>
-        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
-        <h2>Courses</h2>
-        {this.props.loading ? (
-          <Spinner />
-        ) : (
-          <>
-            <button
-              style={{ marginBottom: 20 }}
-              className="btn btn-primary add-course"
-              onClick={() => this.setState({ redirectToAddCoursePage: true })}
-            >
-              Add Course
-            </button>
+  return (
+    <>
+      {redirectToAddCoursePage && <Redirect to="/course" />}
+      <h2>Courses</h2>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <button
+            style={{ marginBottom: 20 }}
+            className="btn btn-primary add-course"
+            onClick={() => setRedirect(true)}
+          >
+            Add Course
+          </button>
 
-            <CourseList
-              onDeleteClick={this.handleDeleteCourse}
-              courses={this.props.courses}
-            />
-          </>
-        )}
-      </>
-    );
-  }
+          <CourseList onDeleteClick={handleDeleteCourse} courses={courses} />
+        </>
+      )}
+    </>
+  );
 }
 
-CoursesPage.propTypes = {
-  authors: PropTypes.array.isRequired,
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
-};
-
-function mapStateToProps(state) {
-  return {
-    courses:
-      state.authors.length === 0
-        ? []
-        : state.courses.map(course => {
-            return {
-              ...course,
-              authorName: state.authors.find(a => a.id === course.authorId).name
-            };
-          }),
-    authors: state.authors,
-    loading: state.apiCallsInProgress > 0
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
-    }
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CoursesPage);
+export default CoursesPage;
